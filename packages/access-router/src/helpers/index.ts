c6337c54154724344a1aa9ce7d8a55dcba5d7563
Junmin Dev <junmin.dev@outlook.com>
2026-05-22 23:07:12 -0700

    refactor: format code style and imports

--- packages/access-router/src/helpers/index.ts
+++ packages/access-router/src/helpers/index.ts
@@ -3,7 +3,16 @@ export * from './document';
 export * from './errors';
 export * from './query';
 
-import { forEach, isArray, isEmpty, isObject, isPlainObject, isString, mapValuesAsync, noop } from '@web-ts-toolkit/utils';
+import {
+  forEach,
+  isArray,
+  isEmpty,
+  isObject,
+  isPlainObject,
+  isString,
+  mapValuesAsync,
+  noop,
+} from '@web-ts-toolkit/utils';
 import { isSchema, isReference } from '../lib';
 import { FilterOperator } from '../enums';
 
--- packages/utils/src/mapValuesAsync.ts
+++ packages/utils/src/mapValuesAsync.ts
@@ -3,7 +3,9 @@ export default async function mapValuesAsync<TObject extends Record<string, unkn
   asyncFn: (value: TObject[keyof TObject], key: string, object: TObject) => Promise<TResult> | TResult,
 ): Promise<Record<string, TResult>> {
   const entries = Object.entries(object) as Array<[string, TObject[keyof TObject]]>;
-  const mapped = await Promise.all(entries.map(async ([key, value]) => [key, await asyncFn(value, key, object)] as const));
+  const mapped = await Promise.all(
+    entries.map(async ([key, value]) => [key, await asyncFn(value, key, object)] as const),
+  );
 
   return Object.fromEntries(mapped);
 }
